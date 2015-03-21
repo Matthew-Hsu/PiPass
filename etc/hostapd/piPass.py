3c8a8e79b5838dd05c4c44acdc8580ca429d510f
Matthew Hsu <matthew.hsu@gmail.com>
2015-03-21 12:09:12 -0700

    More whitespace changes.
    Nothing interesting to see here.

--- etc/hostapd/piPass.py
+++ etc/hostapd/piPass.py
@@ -54,54 +54,54 @@ print("> PiPass is currently running...")
 
 # This loop does not feel pity or remorse or fear and it cannot be stopped unless Half-Life 3 is released.
 while "Waiting for Half-Life 3":
-	# Get all the Nintendo Zones in the collection.
-	zones = collection.getElementsByTagName("ZONE")
+    # Get all the Nintendo Zones in the collection.
+    zones = collection.getElementsByTagName("ZONE")
 
-	# Begin looping through all the Nintendo Zones in the collection.
-	for currentZone in zones:
-		# Open the flag file and read in the value.
-		fo = open(FLAG_PATH)
-		piPassStatus = fo.read()
-		fo.close()
+    # Begin looping through all the Nintendo Zones in the collection.
+    for currentZone in zones:
+        # Open the flag file and read in the value.
+        fo = open(FLAG_PATH)
+        piPassStatus = fo.read()
+        fo.close()
 
-		# If the user has issued an update, then reload current_zones.xml for the updated Nintendo Zones.
-		if piPassStatus == "update\n":
-			# current_zones.xml has been changed, so reload it.
-			DOMTree = xml.dom.minidom.parse(NINTENDO_ZONES)
-			collection = DOMTree.documentElement
+        # If the user has issued an update, then reload current_zones.xml for the updated Nintendo Zones.
+        if piPassStatus == "update\n":
+            # current_zones.xml has been changed, so reload it.
+            DOMTree = xml.dom.minidom.parse(NINTENDO_ZONES)
+            collection = DOMTree.documentElement
 
-			# We want PiPass to keep running.
-			piPassStatus = "execute"
+            # We want PiPass to keep running.
+            piPassStatus = "execute"
 
-			# Overwrite flag file for piPassStatus.
-			fo = open(FLAG_PATH, "wb")
-			fo.write(piPassStatus)
-			fo.close()
+            # Overwrite flag file for piPassStatus.
+            fo = open(FLAG_PATH, "wb")
+            fo.write(piPassStatus)
+            fo.close()
 
-			print("\n< Update Detected! - Using new updated Nintendo Zones. >\n")
+            print("\n< Update Detected! - Using new updated Nintendo Zones. >\n")
 
-			break
+            break
 
-		# Write the current zone information to NETWORK_CONFIGURATION.
-		fo = open(NETWORK_CONFIGURATION, "wb")
+        # Write the current zone information to NETWORK_CONFIGURATION.
+        fo = open(NETWORK_CONFIGURATION, "wb")
 
-		currentMAC = currentZone.getElementsByTagName("MAC")[0]
-		currentMAC = currentMAC.childNodes[0].data
+        currentMAC = currentZone.getElementsByTagName("MAC")[0]
+        currentMAC = currentMAC.childNodes[0].data
 
-		currentSSID = currentZone.getElementsByTagName("SSID")[0]
-		currentSSID = currentSSID.childNodes[0].data
+        currentSSID = currentZone.getElementsByTagName("SSID")[0]
+        currentSSID = currentSSID.childNodes[0].data
 
-		currentDesc = currentZone.getElementsByTagName("DESCRIPTION")[0]
-		currentDesc = currentDesc.childNodes[0].data
+        currentDesc = currentZone.getElementsByTagName("DESCRIPTION")[0]
+        currentDesc = currentDesc.childNodes[0].data
 
-		conf = "interface=wlan0\nbridge=br0\ndriver=nl80211\nssid=" + currentSSID + "\nbssid=" + currentMAC + "\nhw_mode=g\nchannel=6\nauth_algs=1\nwpa=0\nmacaddr_acl=1\naccept_mac_file=/etc/hostapd/mac_accept\nwmm_enabled=0\nignore_broadcast_ssid=0"
+        conf = "interface=wlan0\nbridge=br0\ndriver=nl80211\nssid=" + currentSSID + "\nbssid=" + currentMAC + "\nhw_mode=g\nchannel=6\nauth_algs=1\nwpa=0\nmacaddr_acl=1\naccept_mac_file=/etc/hostapd/mac_accept\nwmm_enabled=0\nignore_broadcast_ssid=0"
 
-		fo.write(conf)
-		fo.close()
+        fo.write(conf)
+        fo.close()
 
-		# Nintendo Zone identity acquired for PiPass spoofing.
-		print("> Spoofing as " + currentMAC + " on " + currentSSID + " ( " + currentDesc + ") for " + str(STREETPASS_CYCLE_MINUTES) + " minute(s).")
+        # Nintendo Zone identity acquired for PiPass spoofing.
+        print("> Spoofing as " + currentMAC + " on " + currentSSID + " ( " + currentDesc + ") for " + str(STREETPASS_CYCLE_MINUTES) + " minute(s).")
 
-		# Run current Nintendo Zone and pause for STREETPASS_CYCLE_MINUTES until moving onto the next Nintendo Zone.
-		subprocess.Popen(COMMAND, shell=True, stdout=subprocess.PIPE)
-		time.sleep(STREETPASS_CYCLE_SECONDS)
+        # Run current Nintendo Zone and pause for STREETPASS_CYCLE_MINUTES until moving onto the next Nintendo Zone.
+        subprocess.Popen(COMMAND, shell=True, stdout=subprocess.PIPE)
+        time.sleep(STREETPASS_CYCLE_SECONDS)
