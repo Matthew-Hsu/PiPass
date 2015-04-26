62125a93f419b73e501acfc8cb7415e2856fa6fd
Matthew Hsu <matthew.hsu@gmail.com>
2015-04-26 01:54:53 -0700

    Caching Disabled
    Disables caching when redirecting to settings.html.

--- var/www/assets/php/settings.php
+++ var/www/assets/php/settings.php
@@ -7,6 +7,9 @@ if ($_POST)
   file_put_contents('/tmp/pipass_config.json', $json);
   exec('sudo cp /tmp/pipass_config.json /var/www/assets/json/');
 
+  header('Cache-Control: no-cache, no-store, must-revalidate');
+  header('Pragma: no-cache');
+  header('Expires: 0');
   header('Location: ../../settings.html');
 }
 
