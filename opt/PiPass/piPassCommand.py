38cf3c7e237030374ec10b799e32426f0cc100e0
David B. Nagle <nagledb@gmail.com>
2015-04-27 22:01:41 +0100

    Updated "update" command to use signal instead of flag file.
    Previously, piPass monitored for an "update" request by reading from a flag
    file on every iteration. This update removes that I/O activity and uses the
    linux signal SIGUSR1 to catch update requests.
    
    In addition to slightly reducing I/O load, this allows a change to the
    STREETPASS_CYCLE_MINUTES setting to take effect for the current zone. If the
    time is reduced below the current elapsed time, it will immediately advance.
    Similarly if the time is extended, that extended time will apply to the current
    zone.

--- opt/PiPass/piPassCommand.py
+++ opt/PiPass/piPassCommand.py
@@ -45,7 +45,7 @@ command = sys.argv[1]
 # Command switchboard for PiPass commands.
 if command == "update":
     # Web GUI: PiPass -> Refresh
-    subprocess.Popen('sudo echo "update" > /tmp/pipass_flag.txt', shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen('sudo pkill --signal SIGUSR1 -f piPass.py', shell=True, stdout=subprocess.PIPE)
     print("Using updated Nintendo Zone list.\n")
 elif command == "stop":
     # Web GUI: PiPass -> Stop
