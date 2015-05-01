0ff65b974928dd53a63640da7125e54d03626bcf
Matthew Hsu <matthew.hsu@gmail.com>
2015-05-01 02:08:09 -0700

    Settings Caching Fix
    Browsers be Bowsers sometimes. Even with caching turned off, not all
    browsers will comply. Instead, I am generating a timestamp and
    appending it to settings.html when settings are saved. Since the URL is
    technically different, browsers are forced to pull new data from the
    server.

--- var/www/assets/php/settings.php
+++ var/www/assets/php/settings.php
@@ -3,14 +3,15 @@
 if ($_POST)
 {
   $json = json_encode($_POST);
+  $date = date_create();
+  $timestamp = date_timestamp_get($date);
+  $settingsURL = "Location: ../../settings.html?time=";
+  $settingsURL .= $timestamp;
 
   file_put_contents('/tmp/pipass_config.json', $json);
   exec('sudo cp /tmp/pipass_config.json /var/www/assets/json/');
 
-  header('Cache-Control: no-cache, no-store, must-revalidate');
-  header('Pragma: no-cache');
-  header('Expires: 0');
-  header('Location: ../../settings.html');
+  header($settingsURL);
 }
 
 ?>
